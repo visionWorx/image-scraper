0685f1692a856326bd089ac7c256000a24cfe0fd
kleingomes <kleingomes@gmail.com>
2018-02-25 18:16:14 -0800

    add image downloader

---
+++ models.js
@@ -0,0 +1,5 @@
+var imageSource = {
+    GOOGLE: 1,
+    DUCKDUCKGO: 2,
+    BING: 3,
+};
--- scraper.js
+++ scraper.js
@@ -1,4 +1,16 @@
 const request = require('request');
 const cheerio = require('cheerio')
+var $ = cheerio
 
+
+downloadImages(['car'])
+
+function downloadImages(images) {
+    images.forEach(function(query) {
+        request('https://duckduckgo.com/?q='+query+'&t=hf&iax=images&ia=images', { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body);
+        });
+    });
+}
 
